0f6ff002865995be239d6a95275006dc9ab45deb
Charlie Clark <charlie.clark@clark-consulting.eu>
2012-06-29 22:41:15 +0200

    Added status.

--- rippy/__init__.py
+++ rippy/__init__.py
@@ -1,1 +1,8 @@
 """A package"""
+
+def status():
+    import random
+    msgs = ['Resting peacefully',
+            'Listening to the angels',
+            "Can't a spirit get any peace around here?"]
+    return random.choice(msgs)
