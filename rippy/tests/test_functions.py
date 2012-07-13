54c360bd9990f142e1dfa8d248e460be8e6be385
Andrew Sawyers <andrew@sawdog.com>
2012-07-13 09:48:15 -0700

    - remove module level import - import test functions in ea. test.

--- rippy/tests/test_functions.py
+++ rippy/tests/test_functions.py
@@ -1,34 +1,36 @@
 from unittest import TestCase
 
-from rippy import RIP
-
 
 class TestFunctions(TestCase):
     """Test the various rst helper functions"""
 
     def test_anchor(self):
         """test the anchor function returns markup as expected"""
+        from rippy.RIP import anchor
         expected = '.. _foo:\n\n'
-        self.assertEqual(RIP.anchor('foo'), expected)
+        self.assertEqual(anchor('foo'), expected)
 
     def test_doc(self):
         """test the doc function returns markup as expected"""
+        from rippy.RIP import doc
         # link with no title...
         expected = ':doc:`foo <foo>`'
-        self.assertEqual(RIP.doc('foo'), expected)
+        self.assertEqual(doc('foo'), expected)
         # link and title are different
         expected = ':doc:`bar <foo>`'
-        self.assertEqual(RIP.doc('foo', 'bar'), expected)
+        self.assertEqual(doc('foo', 'bar'), expected)
 
     def test_download(self):
         """test the download function returns markup as expected"""
+        from rippy.RIP import download
         expected = ':download:`download me <foo>`'
-        self.assertEqual(RIP.download('foo', 'download me'), expected)
+        self.assertEqual(download('foo', 'download me'), expected)
 
     def test_emphasis(self):
         """test the emphasis function returns markup as expected"""
+        from rippy.RIP import emphasis
         expected = '*foo*'
-        self.assertEqual(RIP.emphasis('foo'), expected)
+        self.assertEqual(emphasis('foo'), expected)
 
     def test_header(self):
         """test the header function returns markup as expected for each
@@ -37,31 +39,35 @@ class TestFunctions(TestCase):
            Exceeding 6 causes a TypeError...
 
         """
+        from rippy.RIP import header
         expected = 'foo\n%(level)s\n\n'
-        self.assertEqual(RIP.header('foo', 1), expected % {'level': '###'})
-        self.assertEqual(RIP.header('foo', 2), expected % {'level': '***'})
-        self.assertEqual(RIP.header('foo', 3), expected % {'level': '==='})
-        self.assertEqual(RIP.header('foo', 4), expected % {'level': '---'})
-        self.assertEqual(RIP.header('foo', 5), expected % {'level': '^^^'})
-        self.assertEqual(RIP.header('foo', 6), expected % {'level': '"""'})
-        self.assertRaises(TypeError, RIP.header, 'foo', 7)
+        self.assertEqual(header('foo', 1), expected % {'level': '###'})
+        self.assertEqual(header('foo', 2), expected % {'level': '***'})
+        self.assertEqual(header('foo', 3), expected % {'level': '==='})
+        self.assertEqual(header('foo', 4), expected % {'level': '---'})
+        self.assertEqual(header('foo', 5), expected % {'level': '^^^'})
+        self.assertEqual(header('foo', 6), expected % {'level': '"""'})
+        self.assertRaises(TypeError, header, 'foo', 7)
 
     def test_note(self):
         """test the note function returns markup as expected"""
+        from rippy.RIP import note
         expected = '.. note::\n\n   Notes are snafu.\n'
-        self.assertEqual(RIP.note('Notes are snafu.'), expected)
+        self.assertEqual(note('Notes are snafu.'), expected)
 
     def test_p(self):
         """test the p function returns markup as expected"""
+        from rippy.RIP import p
         expected = '\n\n'
-        self.assertEqual(RIP.p(), expected)
+        self.assertEqual(p(), expected)
 
     def test_ref(self):
         """test the ref function returns markup as expected"""
+        from rippy.RIP import ref
         # just the label to reference
         expected = ':ref:`foo <foo>`'
-        self.assertEqual(RIP.ref('foo'), expected)
+        self.assertEqual(ref('foo'), expected)
         # label and link text are different
         expected = ':ref:`click me <foo>`'
-        self.assertEqual(RIP.ref('foo', 'click me'), expected)
+        self.assertEqual(ref('foo', 'click me'), expected)
 
